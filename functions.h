6507ed6898f752ab3274940c3eb6349458ef13f8
/J_Cole <budoycole123@gmail.com>
2025-05-19 18:36:49 +0800

    Add files via upload

--- functions.h
+++ functions.h
@@ -1,26 +1,32 @@
+#include <windows.h>
+#include <process.h>
+
 void greet(){
 
-    printf("===========================================\n");
-    printf("\tWELCOME TO MY HANG-MAN GAME\n");
-    printf("===========================================\n");
+    printf("\t\t===========================================\n");
+    printf("\t\t\tWELCOME TO MY HANG-MAN GAME\n");
+    printf("\t\t===========================================\n");
 
-    printf("    +---+\n");
-    printf("    |   |\n");
-    printf("    |   O\n");
-    printf("    |  /|\\\n");
-    printf("    |  / \\\n");
-    printf("    |\n");
-    printf("  =========\n");
+    printf("\t  \t  +---+\n");
+    printf("\t  \t  |   |\n");
+    printf("\t  \t  |   O\n");
+    printf("\t  \t  |  /|\\\n");
+    printf("\t  \t  |  / \\\n");
+    printf("\t  \t  |\n");
+    printf("\t  \t=========\n");
 
     printf("INSTRUCTIONS:   The goal of this game is to save the man from hanging!\n");
-    printf("\t\tYour job is to guess the word, if you fail to guess the word within 6 attempts the man will parish!\n");
-    printf("\t\tSo be cautiuos with your decisions and think carefully.\n\n");
+    printf("\t\tIf you fail to guess the word within 6 attempts the man will parish!\n");
+    printf("\t\tSo be cautiuos with your decisions and think carefully.\n");
+
 }
 
+
 void lives(int attempts){
-    
+
+    printf("\n=============================================\n");
     if(attempts == 0){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   \n");
         printf("    |  \n");
@@ -29,7 +35,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 1){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |  \n");
@@ -38,7 +44,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 2){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |   |\n");
@@ -47,7 +53,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 3){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |  /|\n");
@@ -56,7 +62,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 4){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |  /|\\\n");
@@ -65,7 +71,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 5){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |  /|\\\n");
@@ -74,7 +80,7 @@ void lives(int attempts){
         printf("  =========\n");
     }
     if(attempts == 6){
-        printf("    +---+\n");
+        printf("\n    +---+\n");
         printf("    |   |\n");
         printf("    |   O\n");
         printf("    |  /|\\\n");
@@ -82,12 +88,44 @@ void lives(int attempts){
         printf("    |\n");
         printf("  =========\n");
     }
+    
 }
 void displaystatus(char* answer){
-    int i;
 
-    printf("\nGuess the Word: \n");
+    int i; 
+    /*printf("Incorrect letters: ");
+    for(i = 0; i < strlen(mistakes);i++){
+        printf(" %c",mistakes[i]);
+    }printf("\n");*/
+
+    printf("\nGuess the Word: You _____ beyond human capabilities.\n\n");
     for(i = 0; i < strlen(answer);i++){
         printf(" %c",answer[i]);
+    }
+
+
+}
+
+void endGame(char* answer,char* CodeWord, int attempts){
+
+    printf("\n===========================================");
+    if(attempts < 7){
+        printf("\n\n");
+        printf("The word was: ");
+        for(int i = 0; i < strlen(answer); i++){
+            printf(" %c ", answer[i]);
+        }
+        printf("\n\nCongratiolations! You guessed the Word!\n");
+        printf("\n===========================================\n\n\n");
+    }
+    else{
+        printf("\n\n");
+        printf("The word was: ");
+        for(int i = 0; i < strlen(CodeWord); i++){
+            printf(" %c ", CodeWord[i]);
+        }
+
+        printf("\nThe man is Dead : (\n");
+        printf("\n======================================\n\n");
     }
 }
--- main.c
+++ main.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "functions.h"
 #include <stdbool.h>
+#include <conio.h>
+
 
 int main(){
     printf("\n\n");
 
-    char CodeWord[] = "APPLE";
-    char answer[] = "_____";
+    int i;
+    char CodeWord[] = "TRANSCEND";
+    char answer[] = "_________";
+    //char mistakes[] = "_____";
     int attempts = 0;
     char guess;
-    bool correct;
 
     greet();
 
     while(strcmp(answer,CodeWord) != 0 && attempts < 7){
 
+        bool correct = false;
+        
         lives(attempts);
         displaystatus(answer);
 
-        printf("\n\nEnter here: \n");
+        printf("\n\nEnter here: ");
         scanf(" %c",&guess);
+        guess = toupper(guess);
+        printf("\n");
 
-        for(int i = 0; i < strlen(CodeWord);i++){
+        for(i = 0; i < strlen(CodeWord);i++){
             if(guess == CodeWord[i]){
+                answer[i] = guess;
                 correct = true;
             }
-            if(correct){
-                printf("%c",guess);
-            }
+            /*else if(guess != CodeWord[i]){
+                mistakes[i] = guess;
+            }*/
+        }  
+        if(correct){
+            printf("\n\t\t%c is Correct!",guess);
+        }
+        else{
+            printf("\n\t\t%c is Wrong!",guess);            
+            attempts++;
         }
 
-        attempts++;
     }
 
+        if(strcmp(answer,CodeWord)==0 || attempts >=7){
+            endGame(answer,CodeWord,attempts);
+        }
     return 0;
 }
